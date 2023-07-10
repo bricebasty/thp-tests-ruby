e783b9516ac30f54c63c98c0ce2c0abf973ea608
celgost <brice.basty1@gmail.com>
2023-07-10 16:43:47 +0200

    capitalize bonus

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -2,10 +2,13 @@ def translate (string)
   vowels = %w{a e i o u}
   string.gsub(/(\A|\s)\w+/) do |word|
     word.strip!
-    while !vowels.include?(word[0]) || (word[0] == 'u' && word[-1] == 'q')
+    was_capitalized = word[0] == word[0].upcase
+    while !vowels.include?(word[0].downcase) || (word[0].downcase == 'u' && word[-1].downcase == 'q')
       word += word[0]
       word = word[1..-1]
     end
+    word = word.downcase
+    word[0] = word[0].upcase if was_capitalized
     word = ' ' + word + 'ay'
   end.strip
 end
--- spec/06_pig_latin_spec.rb
+++ spec/06_pig_latin_spec.rb
@@ -49,5 +49,9 @@ describe "#translate" do
   # Test-driving bonus:
   # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
   # * retain the punctuation from the original phrase
+  it "capitalizer words are still capitalized (but different initial capital letter)" do
+    s = translate("The Quick Brown Fox")
+    expect(s).to eq("Ethay Ickquay Ownbray Oxfay")
+  end
 
 end
