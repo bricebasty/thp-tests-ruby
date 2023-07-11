112c6f8972a74740bc442bd3ce2dba201561decd
celgost <brice.basty1@gmail.com>
2023-07-11 09:51:55 +0200

    better comments on pig latin

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -9,17 +9,18 @@ end
 # Définition de la méthode "translate_word" qui prend un mot "word" en paramètre
 # et renvoie le mot traduit en pig latin, en utilisant la méthode "process_word" pour le traitement du mot
 def translate_word(word)
-  word.strip!
-  was_capitalized = word[0] == word[0].upcase
-  word = process_word(word.downcase, was_capitalized)
-  " #{word}ay"
+  word.strip!  # Supprime les espaces autour du mot
+  was_capitalized = word[0] == word[0].upcase  # Vérifie si le mot était initialement en majuscule
+  word = process_word(word.downcase, was_capitalized)  # Traite le mot en le convertissant en minuscules et en appliquant les règles spécifiques
+  " #{word}ay"  # Ajoute le suffixe "ay" au mot traduit
 end
 
 # Définition de la méthode "process_word" qui prend un mot "word" et un indicateur "was_capitalized" en paramètres
 # et renvoie le mot traité en pig latin, en prenant en compte les règles spécifiques
 def process_word(word, was_capitalized)
-  vowels = %w[a e i o u]
-  word = word[1..] + word[0] while !vowels.include?(word[0]) || (word[0] == 'u' && word[-1] == 'q')
-  word[0] = word[0].upcase if was_capitalized
-  word
+  vowels = %w[a e i o u]  # Liste des voyelles
+  word = word[1..] + word[0] while !vowels.include?(word[0]) || (word[0] == 'u' && word[-1] == 'q')  # Applique les règles de déplacement des consonnes et des groupes 'qu'
+  word[0] = word[0].upcase if was_capitalized  # Restaure la majuscule si le mot était initialement en majuscule
+  word  # Renvoie le mot traité
 end
+
